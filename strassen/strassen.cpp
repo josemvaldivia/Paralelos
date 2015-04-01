bbe70612db8eed52f8391d78e417547e06d8e6f2
Jose Manuel <jose.valdivia.romero@ucsp.edu.pe>
2015-04-01 01:20:27 -0500

    strassen added

--- strassen/strassen.cpp
+++ strassen/strassen.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>    
 #include <time.h>  
 #include <math.h>
+#include <vector>
 using namespace std;
 
 template <typename T>
@@ -21,6 +22,24 @@ public:
 			data[x]=new T[b];
 		}
 		
+	}
+	Matrix (const Matrix<T> &a)
+	{
+		n=a.n;
+		m=a.m;
+		data= new T*[n];
+		for (int x=0;x<n;x++)
+		{
+			data[x]=new T[m];
+		}
+
+		for (int x=0;x<n;x++)
+		{
+			for (int y=0;y<m;y++)
+			{
+				data[x][y]=a.data[x][y];
+			}
+		}
 	}
 	~Matrix()
 	{
@@ -50,34 +69,119 @@ public:
 			}
 			cout<<"\n";
 		}
+		cout<<"\n\n\n";
 	}
 
-	void sum(Matrix a, Matrix b){
+	Matrix<T> operator + (Matrix<T> a){
 
-		Matrix res (a.n,a.m);
+		Matrix<T> res (a.n,a.m);
 		for (int x=0;x<res.n;x++){
 			for(int y=0;y<res.m;y++){
-				res[x][y]=a[x][y]+b[x][y];
+				res.data[x][y]=a.data[x][y]+data[x][y];
 
 			}
 		}
+		return res;
 	}
 
-	Matrix Normalize(Matrix a)
-	{
-		int new_size=ceil(log2(a.n));
-		Matrix res (new_size<<1,new_size<<1);
-		for (int x =0;x<res.n;x++){
+	Matrix<T> operator - (Matrix<T> a){
+
+		Matrix<T> res (a.n,a.m);
+		for (int x=0;x<res.n;x++){
 			for(int y=0;y<res.m;y++){
-				
+				res.data[x][y]=data[x][y]-a.data[x][y];
+
 			}
 		}
+		return res;
+	}
 
-
-
+	vector<Matrix<T*> > Separate ()
+	{
+		vector<Matrix<T*> > result;
+		int u=n*m/4;
+		int size= ceil(sqrt(u));
+		
+		Matrix<T*> p1(size,size);
+		for (int x=0;x<size;x++)
+		{
+			for (int y=0;y<size;y++)
+			{
+				p1.data[x][y]=&data[x][y];
+			}
+		}
+		Matrix<T*> p2(size,size);
+		int c1=0;
+		int c2=0;
+		for (int x=0;x<size;x++)
+		{
+			c2=0;
+			for (int y=size;y<n;y++)
+			{	
+				p2.data[c1][c2]=&data[x][y];
+				c2++;
+			}
+			c1++;
+		}
+		Matrix<T*> p3(size,size);
+		c1=0;
+		for (int x=size;x<n;x++)
+		{
+			c2=0;
+			for (int y=0;y<size;y++)
+			{
+				p3.data[c1][c2]=&data[x][y];
+				c2++;
+			}
+			c1++;
+		}
+		Matrix<T*> p4(size,size);
+		 c1=0;
+		 
+		for (int x=size;x<n;x++)
+		{
+			c2=0;
+			for (int y=size;y<n;y++)
+			{
+				p4.data[c1][c2]=&data[x][y];
+				c2++;
+			}
+			c1++;
+		}
+		result.push_back(p1);
+		result.push_back(p2);
+		result.push_back(p3);
+		result.push_back(p4);
+		return result;
 
 
 	}
+
+	Matrix<T> strassen (Matrix<T> a)
+	{
+		int n=a.n;
+		Matrix<T> res (n,n);
+		vector<Matrix<T*> > partition_res=res.Separate();
+		if (n==1)
+		{
+			*(res.data[0][0])=((a.data[0][0])) * ((data[0][0]));
+		}
+
+		else
+		{
+			vector<Matrix<T*> > partition_a= a.Separate();
+			vector<Matrix<T*> > partition_self= Separate();
+			partition_res[0]=strassen(partition_a[0],partition_self[0])+strassen(partition_a[1],partition_self[2]);
+			partition_res[1]=strassen(partition_a[0],partition_self[1])+strassen(partition_a[1],partition_self[3]);
+			partition_res[2]=strassen(partition_a[2],partition_self[0])+strassen(partition_a[3],partition_self[2]);
+			partition_res[3]=strassen(partition_a[2],partition_self[1])+strassen(partition_a[3],partition_self[3]);
+
+		}
+		return res;
+
+	}
+	
+
 
 };
 
@@ -91,8 +195,16 @@ int main()
 {
 	srand(time(NULL));
 	Matrix <int> a(4,4);
+	Matrix<int> b(4,4);
 	a.FillWithRandom();
+	b.FillWithRandom();
+
 	a.ShowMatrix();
+	a.strassen(b);
+	cout<<"\n\n\n\n";
+	b.ShowMatrix();
+
+	
 	return 0;
 	
 }
